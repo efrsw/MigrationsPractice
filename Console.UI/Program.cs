d1a0b9045646f993eb23763e9e6db1c390504eaf
Egor Fursov <fursov.egor@gmail.com>
2023-10-17 12:31:26 +0300

    Tried read/write many-to-many entity

--- Console.UI/Program.cs
+++ Console.UI/Program.cs
@@ -4,40 +4,57 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        PopulateData();
+        //PopulateData();
+        AddPostWithTag();
+    }
+    public static void AddPostWithTag()
+    {
+        using var _db = new BlogsContext();
+
+        var tagA = _db.Tags.Where(t => t.Name == "A").First();
+
+        _db.Add(new Post()
+        {
+            Tags = "",
+            PostTags = new List<Tags>() { tagA }
+        });
+        _db.SaveChanges();
     }
 
     public static void PopulateData()
     {
         using var _db = new BlogsContext();
-        Tags a = new Tags()
-        {
-            Name = "A",
-            Posts = new List<Post>()
-        };
 
-        Tags b = new Tags()
-        {
-            Name = "B",
-            Posts = new List<Post>()
-        };
-
-        Post p1 = new Post()
-        {
-            Tags = "",
-            PostTags = new List<Tags>() { a, b }
-        };
-
-        Post p2 = new Post()
+        var post1 = new Post()
         {
             Tags = "",
             PostTags = new List<Tags>()
         };
 
-        b.Posts.Add(p1);
-        a.Posts.Add(p2);
+        var post2 = new Post()
+        {
+            Tags = "",
+            PostTags = new List<Tags>()
+        };
 
-        _db.Posts.Add(p1);
+        var tagA = new Tags()
+        {
+            Name = "A",
+            Posts = new List<Post>() { post1, post2 }
+        };
+
+        var tagB = new Tags()
+        {
+            Name = "B",
+            Posts = new List<Post>() { post1 }
+        };
+
+        post1.PostTags.AddRange(new List<Tags>() { tagA, tagB });
+        post2.PostTags.Add(tagA);
+
+        _db.Add(post1);
+        _db.Add(post2);
+
         _db.SaveChanges();
     }
 }
